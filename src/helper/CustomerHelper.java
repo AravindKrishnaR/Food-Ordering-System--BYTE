db361eb8d9ea76914847f1ba6e5ceb75f05054f0
AravindKrishnaR <aravind2000krishna@gmail.com>
2022-01-31 10:23:38 +0530

    Refactor customer helper class

--- src/helper/CustomerHelper.java
+++ src/helper/CustomerHelper.java
@@ -55,101 +55,17 @@ public class CustomerHelper {
 			int option = sc.nextInt();
 
 			switch (option) {
-			case 1: {
-				System.out.println("\n--- View Food by Restaurant---");
-
-				List<Restaurant> restaurantList = restaurantDao.viewRestaurants();
-
-				System.out.println("\nRestaurant ID \tName \tEmail \tPhone no \tAddress");
-
-				for (Restaurant restaurant : restaurantList) {
-					System.out.println(restaurant.getRestaurantId() + "\t\t" + restaurant.getName() + "\t"
-							+ restaurant.getEmail() + "\t" + restaurant.getPhoneNo() + "\t" + restaurant.getAddress());
-				}
-
-				System.out.print("\nEnter your choice: ");
-				int restaurantChoice = sc.nextInt();
-
-				List<Food> foodMenu = foodDao.viewFoodMenuByRestaurant(restaurantChoice);
-
-				System.out.println("\nFood ID \tName \tDescription \tPrice \tCategory");
-
-				for (Food food : foodMenu) {
-					System.out.println(food.getFoodId() + "\t\t" + food.getName() + "\t" + food.getDescription() + "\t"
-							+ food.getPrice() + "\t" + food.getCategory());
-				}
-
-				System.out.print("\nEnter your food choice to order: ");
-				int foodChoice = sc.nextInt();
-
-				Orders order = new Orders();
-				order.setCustomerUsername(customer.getUsername());
-				order.setRestaurantId(restaurantChoice);
-				order.setFoodId(foodChoice);
-
-				boolean orderStatus = ordersDao.placeOrder(order);
-
-				System.out.println(orderStatus ? "\nOrder successful!" : "\nOrder failed!");
+			case 1:
+				viewFoodMenuByRestaurant(customer);
 				break;
-			}
 
-			case 2: {
-				System.out.println("\n--- View Food by Category---");
-
-				List<String> categoryList = foodDao.viewCategory();
-
-				System.out.println("\nCategories");
-
-				for (String category : categoryList) {
-					System.out.println(category);
-				}
-
-				System.out.print("\nEnter your choice: ");
-				String categoryChoice = sc.next();
-
-				List<Food> foodMenu = foodDao.viewFoodMenuByCategory(categoryChoice);
-				Map<Integer, Restaurant> map = new HashMap<>();
-
-				System.out.println("\nFood ID \tRestaurant \tName \tDescription \tPrice");
-
-				for (Food food : foodMenu) {
-					Restaurant restaurant = new Restaurant();
-					restaurant.setRestaurantId(food.getRestaurantId());
-
-					restaurantDao.getRestaurantName(restaurant);
-					map.put(food.getFoodId(), restaurant);
-
-					System.out.println(food.getFoodId() + "\t\t" + restaurant.getName() + "\t" + food.getName() + "\t"
-							+ food.getDescription() + "\t" + food.getPrice());
-				}
-
-				System.out.print("\nEnter your food choice to order: ");
-				int foodChoice = sc.nextInt();
-
-				Orders order = new Orders();
-				order.setCustomerUsername(customer.getUsername());
-				order.setRestaurantId(map.get(Integer.valueOf(foodChoice)).getRestaurantId());
-				order.setFoodId(foodChoice);
-
-				boolean orderStatus = ordersDao.placeOrder(order);
-
-				System.out.println(orderStatus ? "\nOrder successful!" : "\nOrder failed!");
+			case 2:
+				viewFoodMenuByCategory(customer);
 				break;
-			}
 
-			case 3: {
-				System.out.println("\n--- View Orders---");
-
-				List<Orders> orderList = ordersDao.viewCustomerOrders(customer.getUsername());
-
-				System.out.println("\nFood \tRestaurant \tPrice");
-
-				for (Orders order : orderList) {
-					System.out
-							.println(order.getFoodName() + "\t" + order.getRestaurantName() + "\t" + order.getPrice());
-				}
+			case 3:
+				viewOrders(customer);
 				break;
-			}
 
 			case 4: {
 				optionStatus = false;
@@ -160,6 +76,98 @@ public class CustomerHelper {
 				System.out.println("Enter a valid choice!");
 			}
 			}
+		}
+	}
+
+	private static void viewFoodMenuByRestaurant(Customer customer) {
+		System.out.println("\n--- View Food Menu by Restaurant---");
+
+		List<Restaurant> restaurantList = restaurantDao.viewRestaurants();
+
+		System.out.println("\nRestaurant ID \tName \tEmail \tPhone no \tAddress");
+
+		for (Restaurant restaurant : restaurantList) {
+			System.out.println(restaurant.getRestaurantId() + "\t\t" + restaurant.getName() + "\t"
+					+ restaurant.getEmail() + "\t" + restaurant.getPhoneNo() + "\t" + restaurant.getAddress());
+		}
+
+		System.out.print("\nEnter your choice: ");
+		int restaurantChoice = sc.nextInt();
+
+		List<Food> foodMenu = foodDao.viewFoodMenuByRestaurant(restaurantChoice);
+
+		System.out.println("\nFood ID \tName \tDescription \tPrice \tCategory");
+
+		for (Food food : foodMenu) {
+			System.out.println(food.getFoodId() + "\t\t" + food.getName() + "\t" + food.getDescription() + "\t"
+					+ food.getPrice() + "\t" + food.getCategory());
+		}
+
+		System.out.print("\nEnter your food choice to order: ");
+		int foodChoice = sc.nextInt();
+
+		Orders order = new Orders();
+		order.setCustomerUsername(customer.getUsername());
+		order.setRestaurantId(restaurantChoice);
+		order.setFoodId(foodChoice);
+
+		boolean orderStatus = ordersDao.placeOrder(order);
+
+		System.out.println(orderStatus ? "\nOrder successful!" : "\nOrder failed!");
+	}
+
+	private static void viewFoodMenuByCategory(Customer customer) {
+		System.out.println("\n--- View Food Menu by Category---");
+
+		List<String> categoryList = foodDao.viewCategory();
+
+		System.out.println("\nCategories");
+
+		for (String category : categoryList) {
+			System.out.println(category);
+		}
+
+		System.out.print("\nEnter your choice: ");
+		String categoryChoice = sc.next();
+
+		List<Food> foodMenu = foodDao.viewFoodMenuByCategory(categoryChoice);
+		Map<Integer, Restaurant> map = new HashMap<>();
+
+		System.out.println("\nFood ID \tRestaurant \tName \tDescription \tPrice");
+
+		for (Food food : foodMenu) {
+			Restaurant restaurant = new Restaurant();
+			restaurant.setRestaurantId(food.getRestaurantId());
+
+			restaurantDao.getRestaurantName(restaurant);
+			map.put(food.getFoodId(), restaurant);
+
+			System.out.println(food.getFoodId() + "\t\t" + restaurant.getName() + "\t" + food.getName() + "\t"
+					+ food.getDescription() + "\t" + food.getPrice());
+		}
+
+		System.out.print("\nEnter your food choice to order: ");
+		int foodChoice = sc.nextInt();
+
+		Orders order = new Orders();
+		order.setCustomerUsername(customer.getUsername());
+		order.setRestaurantId(map.get(Integer.valueOf(foodChoice)).getRestaurantId());
+		order.setFoodId(foodChoice);
+
+		boolean orderStatus = ordersDao.placeOrder(order);
+
+		System.out.println(orderStatus ? "\nOrder successful!" : "\nOrder failed!");
+	}
+
+	private static void viewOrders(Customer customer) {
+		System.out.println("\n--- View Orders---");
+
+		List<Orders> orderList = ordersDao.viewCustomerOrders(customer.getUsername());
+
+		System.out.println("\nFood \tRestaurant \tPrice");
+
+		for (Orders order : orderList) {
+			System.out.println(order.getFoodName() + "\t" + order.getRestaurantName() + "\t" + order.getPrice());
 		}
 	}
 
