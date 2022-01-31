86806df80b89fc6d85d149d52f22d02f47cc5ee2
AravindKrishnaR <aravind2000krishna@gmail.com>
2022-01-31 10:13:21 +0530

    Refactor admin helper class

--- src/helper/AdminHelper.java
+++ src/helper/AdminHelper.java
@@ -52,143 +52,155 @@ public class AdminHelper {
 			int option = sc.nextInt();
 
 			switch (option) {
-			case 1: {
-				// Get customer details from the admin
-				System.out.println("\n--- Add Customer ---");
-				Customer customer = new Customer();
-
-				System.out.print("\nEnter username: ");
-				sc.nextLine();
-				customer.setUsername(sc.nextLine());
-
-				System.out.print("Enter password: ");
-				customer.setPassword(sc.nextLine());
-
-				System.out.print("Enter name: ");
-				customer.setName(sc.nextLine());
-
-				System.out.print("Enter email: ");
-				customer.setEmail(sc.nextLine());
-
-				System.out.print("Enter phone number: ");
-				customer.setPhoneNo(sc.nextLine());
-
-				System.out.print("Enter address: ");
-				customer.setAddress(sc.nextLine());
-
-				boolean addCustomerStatus = customerDao.addCustomer(customer);
-
-				System.out.println(addCustomerStatus ? "\nAdd customer successful!" : "\nAdd customer failed!");
+			case 1:
+				addCustomer();
 				break;
-			}
 
-			case 2: {
-				// Get address and username of the customer to be updated
-				System.out.println("\n--- Update Customer ---");
-
-				System.out.print("\nEnter username: ");
-				sc.nextLine();
-				String username = sc.nextLine();
-
-				System.out.print("Enter new address: ");
-				String address = sc.nextLine();
-
-				boolean updateCustomerStatus = customerDao.updateCustomerAddress(username, address);
-
-				System.out
-						.println(updateCustomerStatus ? "\nUpdate customer successful!" : "\nUpdate customer failed!");
+			case 2:
+				updateCustomer();
 				break;
-			}
 
-			case 3: {
-				// Get username of the customer to be deleted
-				System.out.println("\n--- Delete Customer ---");
-
-				System.out.print("\nEnter username: ");
-				sc.nextLine();
-				String username = sc.nextLine();
-
-				boolean deleteCustomerStatus = customerDao.deleteCustomer(username);
-
-				System.out
-						.println(deleteCustomerStatus ? "\nDelete customer successful!" : "\nDelete customer failed!");
+			case 3:
+				deleteCustomer();
 				break;
-			}
 
-			case 4: {
-				// Get restaurant details from the admin
-				System.out.println("\n--- Add Restaurant ---");
-				Restaurant restaurant = new Restaurant();
-
-				System.out.print("\nEnter restaurant id: ");
-				restaurant.setRestaurantId(sc.nextInt());
-				sc.nextLine();
-
-				System.out.print("Enter password: ");
-				restaurant.setPassword(sc.nextLine());
-
-				System.out.print("Enter name: ");
-				restaurant.setName(sc.nextLine());
-
-				System.out.print("Enter email: ");
-				restaurant.setEmail(sc.nextLine());
-
-				System.out.print("Enter phone number: ");
-				restaurant.setPhoneNo(sc.nextLine());
-
-				System.out.print("Enter address: ");
-				restaurant.setAddress(sc.nextLine());
-
-				boolean addRestaurantStatus = restaurantDao.addRestaurant(restaurant);
-
-				System.out.println(addRestaurantStatus ? "\nAdd restaurant successful!" : "\nAdd restaurant failed!");
+			case 4:
+				addRestaurant();
 				break;
-			}
 
-			case 5: {
-				// Get address and username of the restaurant to be updated
-				System.out.println("\n--- Update Restaurant ---");
-
-				System.out.print("\nEnter restaurant id: ");
-				int restaurantId = sc.nextInt();
-				sc.nextLine();
-
-				System.out.print("Enter new address: ");
-				String address = sc.nextLine();
-
-				boolean updateRestaurantStatus = restaurantDao.updateRestaurantAddress(restaurantId, address);
-
-				System.out.println(
-						updateRestaurantStatus ? "\nUpdate restaurant successful!" : "\nUpdate restaurant failed!");
+			case 5:
+				updateRestaurant();
 				break;
-			}
 
-			case 6: {
-				// Get username of the restaurant to be deleted
-				System.out.println("\n--- Delete Restaurant ---");
-
-				System.out.print("\nEnter restaurant id: ");
-				int restaurantId = sc.nextInt();
-				sc.nextLine();
-
-				boolean deleteRestaurantStatus = restaurantDao.deleteRestaurant(restaurantId);
-
-				System.out.println(
-						deleteRestaurantStatus ? "\nDelete restaurant successful!" : "\nDelete restaurant failed!");
+			case 6:
+				deleteRestaurant();
 				break;
-			}
 
-			case 7: {
+			case 7:
 				optionStatus = false;
 				break;
-			}
 
-			default: {
+			default:
 				System.out.println("Enter a valid choice!");
-			}
 			}
 		}
 
+	}
+
+	private static void addCustomer() {
+		// Get customer details from the admin
+		System.out.println("\n--- Add Customer ---");
+		Customer customer = new Customer();
+
+		System.out.print("\nEnter username: ");
+		sc.nextLine();
+		customer.setUsername(sc.nextLine());
+
+		System.out.print("Enter password: ");
+		customer.setPassword(sc.nextLine());
+
+		System.out.print("Enter name: ");
+		customer.setName(sc.nextLine());
+
+		System.out.print("Enter email: ");
+		customer.setEmail(sc.nextLine());
+
+		System.out.print("Enter phone number: ");
+		customer.setPhoneNo(sc.nextLine());
+
+		System.out.print("Enter address: ");
+		customer.setAddress(sc.nextLine());
+
+		boolean addCustomerStatus = customerDao.addCustomer(customer);
+
+		System.out.println(addCustomerStatus ? "\nAdd customer successful!" : "\nAdd customer failed!");
+	}
+
+	private static void updateCustomer() {
+		// Get address and username of the customer to be updated
+		System.out.println("\n--- Update Customer ---");
+
+		System.out.print("\nEnter username: ");
+		sc.nextLine();
+		String username = sc.nextLine();
+
+		System.out.print("Enter new address: ");
+		String address = sc.nextLine();
+
+		boolean updateCustomerStatus = customerDao.updateCustomerAddress(username, address);
+
+		System.out.println(updateCustomerStatus ? "\nUpdate customer successful!" : "\nUpdate customer failed!");
+	}
+
+	private static void deleteCustomer() {
+		// Get username of the customer to be deleted
+		System.out.println("\n--- Delete Customer ---");
+
+		System.out.print("\nEnter username: ");
+		sc.nextLine();
+		String username = sc.nextLine();
+
+		boolean deleteCustomerStatus = customerDao.deleteCustomer(username);
+
+		System.out.println(deleteCustomerStatus ? "\nDelete customer successful!" : "\nDelete customer failed!");
+	}
+
+	private static void addRestaurant() {
+		// Get restaurant details from the admin
+		System.out.println("\n--- Add Restaurant ---");
+		Restaurant restaurant = new Restaurant();
+
+		System.out.print("\nEnter restaurant id: ");
+		restaurant.setRestaurantId(sc.nextInt());
+		sc.nextLine();
+
+		System.out.print("Enter password: ");
+		restaurant.setPassword(sc.nextLine());
+
+		System.out.print("Enter name: ");
+		restaurant.setName(sc.nextLine());
+
+		System.out.print("Enter email: ");
+		restaurant.setEmail(sc.nextLine());
+
+		System.out.print("Enter phone number: ");
+		restaurant.setPhoneNo(sc.nextLine());
+
+		System.out.print("Enter address: ");
+		restaurant.setAddress(sc.nextLine());
+
+		boolean addRestaurantStatus = restaurantDao.addRestaurant(restaurant);
+
+		System.out.println(addRestaurantStatus ? "\nAdd restaurant successful!" : "\nAdd restaurant failed!");
+	}
+
+	private static void updateRestaurant() {
+		// Get address and username of the restaurant to be updated
+		System.out.println("\n--- Update Restaurant ---");
+
+		System.out.print("\nEnter restaurant id: ");
+		int restaurantId = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("Enter new address: ");
+		String address = sc.nextLine();
+
+		boolean updateRestaurantStatus = restaurantDao.updateRestaurantAddress(restaurantId, address);
+
+		System.out.println(updateRestaurantStatus ? "\nUpdate restaurant successful!" : "\nUpdate restaurant failed!");
+	}
+
+	private static void deleteRestaurant() {
+		// Get username of the restaurant to be deleted
+		System.out.println("\n--- Delete Restaurant ---");
+
+		System.out.print("\nEnter restaurant id: ");
+		int restaurantId = sc.nextInt();
+		sc.nextLine();
+
+		boolean deleteRestaurantStatus = restaurantDao.deleteRestaurant(restaurantId);
+
+		System.out.println(deleteRestaurantStatus ? "\nDelete restaurant successful!" : "\nDelete restaurant failed!");
 	}
 
 }
